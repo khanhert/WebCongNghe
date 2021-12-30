a4e2715ccd4618cd821907b815c9454a87740c5c
Ngoc Khanh <khanhert@gmail.com>
2021-12-30 23:50:29 +0700

    nhanh User chua file view ,model

--- chi_tiet_san_pham.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    include('./controllers/c_san_pham.php');
-    $lsp = new San_Pham();
-    $lsp->chi_tiet_san_pham();
-    
-    
-?>
-
--- danh_sach_san_pham_theo_loai.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-include('./controllers/c_san_pham.php');
-$dsl=new San_Pham();
-$dsl->danh_sach_san_pham_theo_loai();
-?>
